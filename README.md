ef992d2494fb84289b2c9e6f8a0c51d9b7a8d3fd
Thiago Cavalcante <thiago.cavalcante@zup.com.br>
2022-04-26 01:59:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CRUD e Atualizações Concorrentes com JPA/Hibernate
+
+## Cadastro de dados únicos com dados sensíveis
+
+Cadastrando reclamações dos usuários
---
+++ src/main/java/br/com/zup/handora/cadastrandoreclamacoes/CadastrandoReclamacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.handora.cadastrandoreclamacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrandoReclamacoesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastrandoReclamacoesApplication.class, args);
+    }
+
+}
